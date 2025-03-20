3b4d4356ded407953d6c595fede852ec177b1497
rodrism3012 <30.rodrigo.12@gmail.com>
2025-03-20 15:01:31 +0100

    added Badge.java: nullability

--- java/tim-from-marketing/src/main/java/Badge.java
+++ java/tim-from-marketing/src/main/java/Badge.java
@@ -1,5 +1,15 @@
 class Badge {
     public String print(Integer id, String name, String department) {
-        
+        String s = "";
+        if (id != null) {
+            s = "[" + id.toString() + "] - ";
+        }
+        s = s + name;
+        if (department != null) {
+            s = s + " - " + department.toUpperCase();
+        } else {
+            s = s + " - OWNER";
+        }
+        return s;
     }
 }
